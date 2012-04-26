679c53fa577e70cf0de1b125c8008c5354d735c8
Guilherme Cavalcanti <guiocavalcanti@gmail.com>
2012-04-26 09:29:44 -0300

    initial commit

---
+++ app/controllers/services_controller.rb
@@ -0,0 +1,19 @@
+class ServicesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def index
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ServiceManager::Application.config.secret_token = '4dc342d7e75b474cd82e350f3841501b83924ecaaee974555df54f86b16da9b3974275bc65dd175ceab3513f9939bd1bab7344b6346130678442c236174fd779'
